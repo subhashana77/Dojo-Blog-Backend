62e11a154b0de0d9c174e6e3ca37d6f25a2eacc1
subhashana77 <106365502+subhashana77@users.noreply.github.com>
2022-06-08 15:54:17 +0530

    error

--- src/main/java/com/arpico/dojoblog/service/impl/AdminServiceImpl.java
+++ src/main/java/com/arpico/dojoblog/service/impl/AdminServiceImpl.java
@@ -29,7 +29,7 @@ import java.util.List;
  */
 
 @Service
-public class AdminServiceImpl implements AdminService , UserDetailsService {
+public class AdminServiceImpl implements AdminService, UserDetailsService {
 
     @Autowired
     private AdminRepo adminRepo;
